3269fa7467629f0d850f9f38a02a4a189e467030
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2023-03-21 15:42:55 +0100

    bundling redux

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,24 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import { ActionType } from './action-types';
+import { bundlerMiddleware } from './middlewares/bundler-middleware';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'code',
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'text',
-  },
-});
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(bundlerMiddleware, thunk)
+);
 
 store.dispatch({
   type: ActionType.INSERT_CELL_AFTER,
@@ -36,3 +25,19 @@ store.dispatch({
     type: 'text',
   },
 });
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_AFTER,
+//   payload: {
+//     id: null,
+//     type: 'code',
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_AFTER,
+//   payload: {
+//     id: null,
+//     type: 'text',
+//   },
+// });
